55b712c697bee84dca90af5e933e2b87ea03acc1
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2024-02-03 08:14:07 +0300

    feat: complete header

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,7 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // Your code here
-    
+var initHeaderMenu = () => {
+  var body = document.body,
+    burger_btn = document.querySelector(".header__burger_btn"),
+    header_menu = document.querySelector(".header__nav");
+
+  if (body && burger_btn && header_menu) {
+    burger_btn.addEventListener("click", function () {
+      body.classList.toggle("lock");
+      burger_btn.classList.toggle("active");
+      header_menu.classList.toggle("active");
+    });
+  } else {
+    console.error("Could not find element(s) for header menu initialization");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  initHeaderMenu();
 });
-
-// Your functions here
-
