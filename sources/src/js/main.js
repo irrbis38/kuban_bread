59201821f9dbacd217dfb49da1041cb0d14441ac
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2024-02-04 21:07:46 +0300

    feat: complete product-presentation page

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -9,11 +9,39 @@ var initHeaderMenu = () => {
       burger_btn.classList.toggle("active");
       header_menu.classList.toggle("active");
     });
+
+    var clearActive = () => {
+      body.classList.remove("lock");
+      burger_btn.classList.remove("active");
+      header_menu.classList.remove("active");
+    };
+
+    var mq1300 = window.matchMedia("(max-width: 1300px)");
+
+    var handleMQ = (e) => {
+      !e.matches && clearActive();
+    };
+
+    mq1300.addEventListener("change", handleMQ);
   } else {
     console.error("Could not find element(s) for header menu initialization");
   }
 };
 
+var initIntroSlider = () => {
+  var swiper = new Swiper(".intro__slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: ".intro__paggination",
+      clickable: true,
+    },
+    autoplay: true,
+  });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   initHeaderMenu();
+  initIntroSlider();
 });
